fe39b5140555813f4b06d22e612ef9d3f5f936bb
Johnnyreyman <sytech@synolviaco.net>
2023-03-28 19:14:53 -0500

    Code refactor after bugs.

--- arch-recovery-chroot.sh
+++ arch-recovery-chroot.sh
@@ -5,28 +5,48 @@ lsblk -o name,type
 echo "Please select a drive to open. SATA /dev/sdX - NVME nvmeXnXp."
 read drive
 
-# Detect the drive format
-if [ -b "$drive" ]; then
-  echo "The format of $drive is: $(blkid -o value -s TYPE $drive)"
-else
+# Verify that the user entered a valid drive name
+if [ ! -b "$drive" ]; then
   echo "Error: $drive is not a valid block device!"
+  exit 1
 fi
+
+# Detect the drive format
+drive_type=$(blkid -o value -s TYPE "$drive")
+
+if [ -z "$drive_type" ]; then
+  echo "Error: Could not determine drive format for $drive."
+  exit 1
+fi
+
+echo "Drive format: $drive_type"
 
 echo "Is this drive encrypted?"
 read luks
 
 if [ "$luks" == "y" ]; then
-    sudo cryptsetup luksOpen $drive luks_drive
-    if [ "$(blkid -o value -s TYPE $drive)" == "btrfs" ]; then
-    sudo mount $drive'1' /mnt/boot/efi
+  if ! command -v cryptsetup &> /dev/null; then
+    echo "Error: cryptsetup is not installed."
+    exit 1
+  fi
+
+  sudo cryptsetup luksOpen "$drive" luks_drive
+
+  if [ "$drive_type" == "btrfs" ]; then
+    sudo mount "${drive}1" /mnt/boot/efi
     sudo mount -t btrfs --subvol=@ /dev/mapper/luks_drive /mnt
-    fi
+  fi
+elif [ "$luks" != "n" ]; then
+  echo "Error: Invalid input for encrypted drive question."
+  exit 1
+else
+  sudo mount "${drive}2" /mnt
+  sudo mount "${drive}1" /mnt/boot/efi
 fi
 
-if [ "$luks" == "n"]; then
-    sudo mount $drive'2' /mnt
-    sudo mount $drive'1' /mnt/boot/efi
+if ! command -v arch-chroot &> /dev/null; then
+  echo "Error: arch-chroot is not installed."
+  exit 1
 fi
 
 sudo arch-chroot /mnt
-
