f11f759994feb54306e8907c0258130a1d15e518
Johnnyreyman <sytech@synolviaco.net>
2023-03-28 20:29:48 -0500

    Fix 3

--- fix-the-computer.sh
+++ fix-the-computer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo cryptsetup luksOpen /dev/nvme0n1p2 enterprise
-cd /mnt
-sudo mount -t btrfs -o subvol=@ /dev/mapper/enterprise /mnt
-sudo mkdir boot/efi
+cd /mnt2
+sudo mount -t btrfs -o subvol=@ /dev/mapper/enterprise /mnt2
+sudo mkdir /mnt2/boot/efi
 sudo mount /dev/nvme0n1p1 /boot/efi
 sudo arch-chroot /mnt
